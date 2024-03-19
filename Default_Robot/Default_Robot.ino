9e91c14e75711e50792deb8c8ad1c86d5432b42f
JAbdelmalek1 <jabdelmalek@hmc.edu>
2024-03-18 23:34:26 -0700

    fix

--- Default_Robot/Default_Robot.ino
+++ Default_Robot/Default_Robot.ino
@@ -109,7 +109,7 @@ void loop() {
   if ( currentTime-printer.lastExecutionTime > LOOP_PERIOD ) {
     printer.lastExecutionTime = currentTime;
     printer.printValue(0,adc.printSample());
-    printer.printValue(1,button_sampler.printState());
+    printer.printValue(1,ef.printStates());
     printer.printValue(2,logger.printState());
     printer.printValue(3,gps.printState());   
     printer.printValue(4,state_estimator.printState());     
